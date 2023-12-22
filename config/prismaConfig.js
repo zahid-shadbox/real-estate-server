f87b886cc46d55cf6c0097af45989f26e92aa779
zahid-shadbox <zahidshadbox@gmail.com>
2023-12-22 19:42:45 +0530

    added console log for DB connection with prisma

--- config/prismaConfig.js
+++ config/prismaConfig.js
@@ -1,4 +1,18 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
 
-export {prisma}
+const prisma = new PrismaClient();
+
+async function main() {
+ try {
+ await prisma.$connect();
+ console.log('Connected to the database');
+ } catch (err) {
+ console.error('Failed to connect to the database: ', err);
+ } finally {
+ await prisma.$disconnect();
+ }
+}
+
+main().catch(e => { throw e });
+
+export { prisma };
